e9aacdeeb7d3baf7095331b05373e4d5544b37f6
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-08-29 20:15:59 -0500

    more lint changes, nothing to see here...

--- stylelint.config.js
+++ stylelint.config.js
@@ -20,7 +20,6 @@ module.exports = {
     "block-opening-brace-newline-after": "always-multi-line",
     "block-opening-brace-space-after": "always-single-line",
     "block-opening-brace-space-before": "always",
-    "color-hex-case": "lower",
     "color-hex-length": "short",
     "comment-empty-line-before": [ "always", {
       except: ["first-nested"],
